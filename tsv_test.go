0abf23a4e4fd2a8d2ad1736b0b774736a7e8335a
phuslu <phuslu@hotmail.com>
2019-07-12 22:41:51 +0800

    more tsv coverage

--- tsv_test.go
+++ tsv_test.go
@@ -9,13 +9,22 @@ func TestTSVLogger(t *testing.T) {
 		Separator: ',',
 		Writer:    &Writer{},
 	}
+
 	log.New().
 		Timestamp().
 		Bool(true).
-		Float64(1.111).
-		Float64(1.111).
-		Int64(1234567890).
-		Int32(123456).
+		Float64(0.618).
+		Int64(123).
+		Uint64(456).
+		Float32(12.0).
+		Int(42).
+		Int32(42).
+		Int16(42).
+		Int8(42).
+		Uint32(42).
+		Uint16(42).
+		Uint8(42).
+		Bytes([]byte("\"<,\t>?'")).
 		Str("\"<,\t>?'").
 		Send()
 }
