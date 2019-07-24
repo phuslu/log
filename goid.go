67aba298d56226078910ad74489ee0b215f2dd1f
phuslu <phuslu@hotmail.com>
2019-07-24 10:41:38 +0800

    add fallback version for goid

--- goid.go
+++ goid.go
@@ -2,6 +2,19 @@
 
 package log
 
-func goid() int64 {
-	return 0
+import (
+	"runtime"
+)
+
+func goid() (n int64) {
+	const offset = len("goroutine ")
+	var data [64]byte
+	b := data[:runtime.Stack(data[:], false)]
+	if len(b) <= offset {
+		return
+	}
+	for i := offset; b[i] != ' '; i++ {
+		n = n*10 + int64(b[i]-'0')
+	}
+	return n
 }
