fb500dbe0941919d8e8d8d1623e2b7fce9dd7234
phuslu <phuslu@hotmail.com>
2019-07-28 21:10:39 +0800

    rename ioctl control to req

--- console_vt100.go
+++ console_vt100.go
@@ -14,29 +14,29 @@ func (w *ConsoleWriter) Write(p []byte) (int, error) {
 }
 
 func IsTerminal(file *os.File) bool {
-	var control uintptr
+	var req uintptr
 	switch runtime.GOOS {
 	case "linux":
 		switch runtime.GOARCH {
 		case "ppc64", "ppc64le":
-			control = 0x402c7413
+			req = 0x402c7413
 		case "mips", "mipsle", "mips64", "mips64le":
-			control = 0x540d
+			req = 0x540d
 		default:
-			control = 0x5401
+			req = 0x5401
 		}
 	case "darwin":
 		switch runtime.GOARCH {
 		case "amd64", "arm64":
-			control = 0x40487413
+			req = 0x40487413
 		default:
-			control = 0x402c7413
+			req = 0x402c7413
 		}
 	default:
-		control = 0x402c7413
+		req = 0x402c7413
 	}
 
 	var termios [256]byte
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, file.Fd(), control, uintptr(unsafe.Pointer(&termios[0])), 0, 0, 0)
+	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, file.Fd(), req, uintptr(unsafe.Pointer(&termios[0])), 0, 0, 0)
 	return err == 0
 }
