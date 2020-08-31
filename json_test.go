1b182220e3888bf001007ee5b27d6bfce51a9441
phuslu <phus.lu@gmail.com>
2020-08-31 18:18:47 +0800

    improve json coverage

--- json_test.go
+++ json_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestJsonParse(t *testing.T) {
-	s := `{"time":"2019-07-10T05:35:54.277Z","level":"info","caller":"pretty.go:42","error":"这是一个错误哦\n","foo":"bar","n":42,"t":true,"f":false,"o":null,"a":[1,2,3],"obj":{"a":[1,2], "b":{"c":3}},"message":"hello json console color writer\t123"}`
+	s := `{"time":"2019-07-10T05:35:54.277Z","level":"info","caller":"pretty.go:42","error":"这是一个🌐哦\n","foo":"bar","n":42,"t":true,"f":false,"o":null,"a":[1,2,3],"obj":{"a":[1,2], "b":{"c":3}},"message":"hello json console color writer\t123"}`
 
 	results, err := jsonParse([]byte(s))
 	if err != nil {
