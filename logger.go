02bbebcbc49785f6a9ea6ff0a0a6e09a1a97a5fd
phuslu <phus.lu@gmail.com>
2021-02-13 23:14:53 +0800

    manual inlining time field formating for header

--- logger.go
+++ logger.go
@@ -307,13 +307,129 @@ func (l *Logger) header(level Level) *Entry {
 		e.buf = append(e.buf, l.TimeField...)
 		e.buf = append(e.buf, '"', ':')
 	}
+	sec, nsec := walltime()
 	switch l.TimeFormat {
 	case "":
-		e.rfc3339(walltime())
+		var tmp [32]byte
+		var buf []byte
+		if timeOffset == 0 {
+			// "2006-01-02T15:04:05.999Z"
+			tmp[25] = '"'
+			tmp[24] = 'Z'
+			buf = tmp[:26]
+		} else {
+			// "2006-01-02T15:04:05.999Z07:00"
+			tmp[30] = '"'
+			tmp[29] = timeZone[5]
+			tmp[28] = timeZone[4]
+			tmp[27] = timeZone[3]
+			tmp[26] = timeZone[2]
+			tmp[25] = timeZone[1]
+			tmp[24] = timeZone[0]
+			buf = tmp[:31]
+		}
+		// date time
+		sec += 9223372028715321600 + timeOffset // unixToInternal + internalToAbsolute + timeOffset
+		year, month, day, _ := absDate(uint64(sec), true)
+		hour, minute, second := absClock(uint64(sec))
+		// year
+		a := year / 100 * 2
+		b := year % 100 * 2
+		tmp[0] = '"'
+		tmp[1] = smallsString[a]
+		tmp[2] = smallsString[a+1]
+		tmp[3] = smallsString[b]
+		tmp[4] = smallsString[b+1]
+		// month
+		month *= 2
+		tmp[5] = '-'
+		tmp[6] = smallsString[month]
+		tmp[7] = smallsString[month+1]
+		// day
+		day *= 2
+		tmp[8] = '-'
+		tmp[9] = smallsString[day]
+		tmp[10] = smallsString[day+1]
+		// hour
+		hour *= 2
+		tmp[11] = 'T'
+		tmp[12] = smallsString[hour]
+		tmp[13] = smallsString[hour+1]
+		// minute
+		minute *= 2
+		tmp[14] = ':'
+		tmp[15] = smallsString[minute]
+		tmp[16] = smallsString[minute+1]
+		// second
+		second *= 2
+		tmp[17] = ':'
+		tmp[18] = smallsString[second]
+		tmp[19] = smallsString[second+1]
+		// milli seconds
+		a = int(nsec) / 1000000
+		b = a % 100 * 2
+		tmp[20] = '.'
+		tmp[21] = byte('0' + a/100)
+		tmp[22] = smallsString[b]
+		tmp[23] = smallsString[b+1]
+		// append to e.buf
+		e.buf = append(e.buf, buf...)
 	case TimeFormatUnix:
-		e.unix(walltime())
+		// 1595759807
+		var tmp [10]byte
+		// seconds
+		b := sec % 100 * 2
+		sec /= 100
+		tmp[9] = smallsString[b+1]
+		tmp[8] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[7] = smallsString[b+1]
+		tmp[6] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[5] = smallsString[b+1]
+		tmp[4] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[3] = smallsString[b+1]
+		tmp[2] = smallsString[b]
+		b = sec % 100 * 2
+		tmp[1] = smallsString[b+1]
+		tmp[0] = smallsString[b]
+		// append to e.buf
+		e.buf = append(e.buf, tmp[:]...)
 	case TimeFormatUnixMs:
-		e.unixms(walltime())
+		// 1595759807105
+		var tmp [13]byte
+		// milli seconds
+		a := int64(nsec) / 1000000
+		b := a % 100 * 2
+		tmp[12] = smallsString[b+1]
+		tmp[11] = smallsString[b]
+		tmp[10] = byte('0' + a/100)
+		// seconds
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[9] = smallsString[b+1]
+		tmp[8] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[7] = smallsString[b+1]
+		tmp[6] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[5] = smallsString[b+1]
+		tmp[4] = smallsString[b]
+		b = sec % 100 * 2
+		sec /= 100
+		tmp[3] = smallsString[b+1]
+		tmp[2] = smallsString[b]
+		b = sec % 100 * 2
+		tmp[1] = smallsString[b+1]
+		tmp[0] = smallsString[b]
+		// append to e.buf
+		e.buf = append(e.buf, tmp[:]...)
 	default:
 		e.buf = append(e.buf, '"')
 		e.buf = timeNow().AppendFormat(e.buf, l.TimeFormat)
@@ -404,66 +520,6 @@ func (e *Entry) rfc3339(sec int64, nsec int32) {
 	tmp[23] = smallsString[b+1]
 	// append to e.buf
 	e.buf = append(e.buf, buf...)
-}
-
-func (e *Entry) unix(sec int64, _ int32) {
-	// 1595759807
-	var tmp [10]byte
-	// seconds
-	b := sec % 100 * 2
-	sec /= 100
-	tmp[9] = smallsString[b+1]
-	tmp[8] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[7] = smallsString[b+1]
-	tmp[6] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[5] = smallsString[b+1]
-	tmp[4] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[3] = smallsString[b+1]
-	tmp[2] = smallsString[b]
-	b = sec % 100 * 2
-	tmp[1] = smallsString[b+1]
-	tmp[0] = smallsString[b]
-	// append to e.buf
-	e.buf = append(e.buf, tmp[:]...)
-}
-
-func (e *Entry) unixms(sec int64, nsec int32) {
-	// 1595759807105
-	var tmp [13]byte
-	// milli seconds
-	a := int64(nsec) / 1000000
-	b := a % 100 * 2
-	tmp[12] = smallsString[b+1]
-	tmp[11] = smallsString[b]
-	tmp[10] = byte('0' + a/100)
-	// seconds
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[9] = smallsString[b+1]
-	tmp[8] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[7] = smallsString[b+1]
-	tmp[6] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[5] = smallsString[b+1]
-	tmp[4] = smallsString[b]
-	b = sec % 100 * 2
-	sec /= 100
-	tmp[3] = smallsString[b+1]
-	tmp[2] = smallsString[b]
-	b = sec % 100 * 2
-	tmp[1] = smallsString[b+1]
-	tmp[0] = smallsString[b]
-	// append to e.buf
-	e.buf = append(e.buf, tmp[:]...)
 }
 
 // Time append append t formated as string using time.RFC3339Nano.
