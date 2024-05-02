5daf9044efbab075066453ed4ae4fb329915dd8c
phuslu <phus.lu@gmail.com>
2024-05-02 22:56:36 +0800

    refactor slog handler adapter

--- logger_slog.go
+++ logger_slog.go
@@ -6,7 +6,6 @@ package log
 import (
 	"context"
 	"log/slog"
-	"sync"
 )
 
 func slogAttrEval(e *Entry, a slog.Attr) *Entry {
@@ -46,62 +45,41 @@ func slogAttrEval(e *Entry, a slog.Attr) *Entry {
 	}
 }
 
-type slogGroup struct {
-	name  string
-	attrs []slog.Attr
-	child *slogGroup
-}
-
-func (group *slogGroup) empty() bool {
-	return len(group.attrs) == 0 && group.child == nil
-}
-
-func (group *slogGroup) WithAttrs(attrs []slog.Attr) {
-	if group.child == nil {
-		group.attrs = append([]slog.Attr{}, group.attrs...)
-		group.attrs = append(group.attrs, attrs...)
-	} else {
-		group.child.WithAttrs(attrs)
-	}
-}
-
-func (group *slogGroup) WithGroup(name string) {
-	if group.child == nil {
-		group.child = &slogGroup{name: name}
-	} else {
-		group.child.WithGroup(name)
-	}
-}
-
-func (group *slogGroup) Eval(e *Entry) *Entry {
-	for _, attr := range group.attrs {
-		e = slogAttrEval(e, attr)
-	}
-	if group.child != nil {
-		e = e.Dict(group.child.name, group.child.Eval(NewContext(nil)).Value())
-	}
-	return e
-}
-
 type slogHandler struct {
-	logger  Logger
-	caller  int
-	group   slogGroup
-	once    sync.Once
-	context Context
+	logger   Logger
+	caller   int
+	grouping bool
+	groups   int
+	entry    Entry
 }
 
-// nolint:govet // disable copylocks lint
 func (h slogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	h.group.WithAttrs(attrs)
-	h.once = sync.Once{}
+	if len(attrs) == 0 {
+		return &h
+	}
+	i := len(h.entry.buf)
+	for _, attr := range attrs {
+		h.entry = *slogAttrEval(&h.entry, attr)
+	}
+	if h.grouping {
+		h.entry.buf[i] = '{'
+	}
+	h.grouping = false
 	return &h
 }
 
-// nolint:govet // disable copylocks lint
 func (h slogHandler) WithGroup(name string) slog.Handler {
 	if name != "" {
-		h.group.WithGroup(name)
+		if h.grouping {
+			h.entry.buf = append(h.entry.buf, '{')
+		} else {
+			h.entry.buf = append(h.entry.buf, ',')
+		}
+		h.entry.buf = append(h.entry.buf, '"')
+		h.entry.buf = append(h.entry.buf, name...)
+		h.entry.buf = append(h.entry.buf, '"', ':')
+		h.grouping = true
+		h.groups++
 	}
 	return &h
 }
@@ -139,18 +117,53 @@ func (h *slogHandler) Handle(_ context.Context, r slog.Record) error {
 		e.caller(1, r.PC, h.caller < 0)
 	}
 
-	if !h.group.empty() {
-		h.once.Do(func() {
-			h.context = h.group.Eval(NewContext(nil)).Value()
-		})
-		e = e.Context(h.context)
-	}
+	// msg
+	e = e.Str("message", r.Message)
 
+	// with
+	if b := h.entry.buf; len(b) != 0 {
+		e = e.Context(b)
+	}
+	i := len(e.buf)
+
+	// attrs
 	r.Attrs(func(attr slog.Attr) bool {
 		e = slogAttrEval(e, attr)
 		return true
 	})
-	e.Msg(r.Message)
+
+	lastindex := func(buf []byte) int {
+		for i := len(buf) - 3; i >= 1; i-- {
+			if buf[i] == '"' && (buf[i-1] == ',' || buf[i-1] == '{') {
+				return i
+			}
+		}
+		return -1
+	}
+
+	// group attrs
+	if h.grouping {
+		if r.NumAttrs() > 0 {
+			e.buf[i] = '{'
+		} else if i = lastindex(e.buf); i > 0 {
+			e.buf = e.buf[:i-1]
+			h.groups--
+			for e.buf[len(e.buf)-1] == ':' {
+				if i = lastindex(e.buf); i > 0 {
+					e.buf = e.buf[:i-1]
+					h.groups--
+				}
+			}
+		} else {
+			e.buf = append(e.buf, '{')
+		}
+	}
+
+	for i := 0; i < h.groups; i++ {
+		e.buf = append(e.buf, '}')
+	}
+
+	e.Msg("")
 	return nil
 }
 
