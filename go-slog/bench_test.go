de1fd635cec1780fe43f17ceef0c408d0e010f13
phuslu <phus.lu@gmail.com>
2024-05-03 20:59:52 +0800

    add slog/JSONHandler to go-slog bench

--- go-slog/bench_test.go
+++ go-slog/bench_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/madkins23/go-slog/bench/tests"
 )
 
+// BenchmarkSlogJSON runs benchmarks for the slog/JSONHandler JSON handler.
+func BenchmarkSlogJSON(b *testing.B) {
+	slogSuite := tests.NewSlogBenchmarkSuite(creatorSlogJson())
+	tests.Run(b, slogSuite)
+}
+
 // BenchmarkPhusluSlog runs benchmarks for the phuslu/slog handler.
 func BenchmarkPhusluSlog(b *testing.B) {
 	slogSuite := tests.NewSlogBenchmarkSuite(creatorPhusluSlog())
--- go-slog/creator.go
+++ go-slog/creator.go
@@ -8,15 +8,34 @@ import (
 	"github.com/phuslu/log"
 )
 
-// creatorPhusluSlog returns a PhusluSlog object for the [phuslu/slog] handler.
+// creatorSlogJson returns a Creator object for the [slog/JSONHandler] handler.
+//
+// [slog/JSONHandler]: https://pkg.go.dev/log/slog#JSONHandler
+func creatorSlogJson() infra.Creator {
+	return infra.NewCreator("slog/JSONHandler", creatorSlogJsonHandlerFn, nil,
+		`^slog/JSONHandler^ is the JSON handler provided with the ^slog^ library.
+		It is fast and as a part of the Go distribution it is used
+		along with published documentation as a model for ^slog.Handler^ behavior.`,
+		map[string]string{
+			"slog/JSONHandler": "https://pkg.go.dev/log/slog#JSONHandler",
+		})
+}
+
+func creatorSlogJsonHandlerFn(w io.Writer, options *slog.HandlerOptions) slog.Handler {
+	return slog.NewJSONHandler(w, options)
+}
+
+// creatorPhusluSlog returns a Creator object for the [phuslu/slog] handler.
+//
+// [phuslu/slog]: https://github.com/phuslu/log/blob/master/slog.go
 func creatorPhusluSlog() infra.Creator {
-	return infra.NewCreator("phuslu/slog", creatorPhusluSloghandlerFn, nil,
+	return infra.NewCreator("phuslu/slog", creatorPhusluSlogHandlerFn, nil,
 		`^phuslu/slog^ is a wrapper around the pre-existing ^phuslu/log^ logging library.`,
 		map[string]string{
 			"phuslu/log": "https://github.com/phuslu/log",
 		})
 }
 
-func creatorPhusluSloghandlerFn(w io.Writer, options *slog.HandlerOptions) slog.Handler {
+func creatorPhusluSlogHandlerFn(w io.Writer, options *slog.HandlerOptions) slog.Handler {
 	return log.SlogNewJSONHandler(w, options)
 }
