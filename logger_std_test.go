8f028dd24de0157b68c4393feac0f8b159bcd776
phuslu <phus.lu@gmail.com>
2024-05-03 11:36:01 +0800

    rename slog file names

--- logger_std_test.go
+++ logger_std_test.go
@@ -1,13 +1,17 @@
+//go:build go1.21
+// +build go1.21
+
 package log
 
 import (
 	"fmt"
 	stdLog "log"
+	"log/slog"
 	"os"
 	"testing"
 )
 
-func TestStdWriter(t *testing.T) {
+func TestStdLogWriter(t *testing.T) {
 	w := &stdLogWriter{
 		Logger: Logger{
 			Level:  InfoLevel,
@@ -20,7 +24,7 @@ func TestStdWriter(t *testing.T) {
 	fmt.Fprintf(w, "hello from stdLog debug %s", "Printf")
 }
 
-func TestStdLogger(t *testing.T) {
+func TestStdLogLogger(t *testing.T) {
 	logger := Logger{
 		Level:   DebugLevel,
 		Caller:  -1,
@@ -37,4 +41,52 @@ func TestStdLogger(t *testing.T) {
 	stdLog.Print("hello from stdLog Print")
 	stdLog.Println("hello from stdLog Println")
 	stdLog.Printf("hello from stdLog %s", "Printf")
+}
+
+func TestStdSlogNormal(t *testing.T) {
+	var logger *slog.Logger = (&Logger{
+		Level:      InfoLevel,
+		TimeField:  "date",
+		TimeFormat: "2006-01-02",
+		Caller:     1,
+	}).Slog()
+
+	logger.Info("hello from slog Info")
+	logger.Warn("hello from slog Warn")
+	logger.Error("hello from slog Error")
+}
+
+func TestStdSlogAttrs(t *testing.T) {
+	var logger *slog.Logger = (&Logger{
+		Level:      InfoLevel,
+		TimeField:  "date",
+		TimeFormat: "2006-01-02",
+		Caller:     -1,
+	}).Slog()
+
+	sublogger := logger.With("logger_name", "sub_logger").WithGroup("g").With("everything", 42)
+	sublogger.Info("hello from sub attr slog")
+	logger.Info("hello from origin slog")
+}
+
+func TestStdSlogGroup(t *testing.T) {
+	var logger *slog.Logger = (&Logger{
+		Level:      InfoLevel,
+		TimeField:  "date",
+		TimeFormat: "2006-01-02",
+		Caller:     1,
+	}).Slog()
+
+	logger1 := logger.WithGroup("g").With("1", "2").With("3", "4")
+	logger1.Info("hello from group slog 1")
+	logger1.Info("hello from group slog 2")
+
+	logger2 := logger1.WithGroup("g1").With("a", "b").With("c", "d").
+		WithGroup("g2").With("foo", "bar").With("bar", "foo").
+		WithGroup("g3").With("x", 1).With("y", 2).With("z", 3)
+	logger2.Info("hello from group slog 3")
+	logger2.Info("hello from group slog 4")
+
+	logger1.Info("hello from group slog 1")
+	logger1.Info("hello from group slog 2")
 }
--- slog_test.go
+++ slog_test.go
@@ -9,54 +9,6 @@ import (
 	"testing"
 )
 
-func TestSlogNormal(t *testing.T) {
-	var logger *slog.Logger = (&Logger{
-		Level:      InfoLevel,
-		TimeField:  "date",
-		TimeFormat: "2006-01-02",
-		Caller:     1,
-	}).Slog()
-
-	logger.Info("hello from slog Info")
-	logger.Warn("hello from slog Warn")
-	logger.Error("hello from slog Error")
-}
-
-func TestSlogAttrs(t *testing.T) {
-	var logger *slog.Logger = (&Logger{
-		Level:      InfoLevel,
-		TimeField:  "date",
-		TimeFormat: "2006-01-02",
-		Caller:     -1,
-	}).Slog()
-
-	sublogger := logger.With("logger_name", "sub_logger").WithGroup("g").With("everything", 42)
-	sublogger.Info("hello from sub attr slog")
-	logger.Info("hello from origin slog")
-}
-
-func TestSlogGroup(t *testing.T) {
-	var logger *slog.Logger = (&Logger{
-		Level:      InfoLevel,
-		TimeField:  "date",
-		TimeFormat: "2006-01-02",
-		Caller:     1,
-	}).Slog()
-
-	logger1 := logger.WithGroup("g").With("1", "2").With("3", "4")
-	logger1.Info("hello from group slog 1")
-	logger1.Info("hello from group slog 2")
-
-	logger2 := logger1.WithGroup("g1").With("a", "b").With("c", "d").
-		WithGroup("g2").With("foo", "bar").With("bar", "foo").
-		WithGroup("g3").With("x", 1).With("y", 2).With("z", 3)
-	logger2.Info("hello from group slog 3")
-	logger2.Info("hello from group slog 4")
-
-	logger1.Info("hello from group slog 1")
-	logger1.Info("hello from group slog 2")
-}
-
 func TestSlogJsonHandler(t *testing.T) {
 	logger := slog.New(SlogNewJSONHandler(os.Stderr, &slog.HandlerOptions{AddSource: false}))
 
