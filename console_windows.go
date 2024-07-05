28622bc2cd647b4230ae1c9c88650971399aea78
phuslu <phus.lu@gmail.com>
2024-07-05 13:34:41 +0800

    rename SetConsoleMode for windows console writer

--- console_windows.go
+++ console_windows.go
@@ -21,9 +21,9 @@ func isTerminal(fd uintptr, _, _ string) bool {
 }
 
 var (
-	kernel32                = syscall.NewLazyDLL("kernel32.dll")
-	setConsoleMode          = kernel32.NewProc("SetConsoleMode").Call
-	setConsoleTextAttribute = kernel32.NewProc("SetConsoleTextAttribute").Call
+	kernel32                    = syscall.NewLazyDLL("kernel32.dll")
+	procSetConsoleMode          = kernel32.NewProc("SetConsoleMode").Call
+	procSetConsoleTextAttribute = kernel32.NewProc("SetConsoleTextAttribute").Call
 
 	muConsole   sync.Mutex
 	onceConsole sync.Once
@@ -74,8 +74,8 @@ func (w *ConsoleWriter) writew(out io.Writer, p []byte) (n int, err error) {
 	// color print
 	var cprint = func(color uintptr, b []byte) {
 		if color != White {
-			setConsoleTextAttribute(uintptr(syscall.Stderr), color)
-			defer setConsoleTextAttribute(uintptr(syscall.Stderr), White)
+			procSetConsoleTextAttribute(uintptr(syscall.Stderr), color)
+			defer procSetConsoleTextAttribute(uintptr(syscall.Stderr), White)
 		}
 		var i int
 		i, err = out.Write(b)
@@ -183,6 +183,6 @@ func isVirtualTerminal() bool {
 	}
 
 	// enable ENABLE_VIRTUAL_TERMINAL_PROCESSING
-	ret, _, _ := setConsoleMode(uintptr(syscall.Stderr), uintptr(n|0x4))
+	ret, _, _ := procSetConsoleMode(uintptr(syscall.Stderr), uintptr(n|0x4))
 	return ret != 0
 }
