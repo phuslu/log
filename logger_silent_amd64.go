aa7d647cfdc491e48f5c46a24c2ad6d51919c3f7
phuslu <phus.lu@gmail.com>
2024-07-07 01:00:32 +0800

    add tweets for logger_silent_amd64.go

--- logger_silent_amd64.go
+++ logger_silent_amd64.go
@@ -2,6 +2,12 @@
 
 package log
 
+/*
+   Avoid atomic overhead by referencing these tweets:
+   https://x.com/i/status/1782634032884593099
+   https://x.com/i/status/1782635198930358747
+*/
+
 //gcassert:inline
 func (l *Logger) silent(level Level) bool {
 	return level < l.Level
