0641cdd1f8376ea326f7db7f2ba9186e1fc60d24
Roland Lammel <rl@neotel.at>
2025-02-22 20:13:48 +0100

    Add categorized loggers

--- logger_test.go
+++ logger_test.go
@@ -588,6 +588,42 @@ func (e errno) Format(s fmt.State, verb rune) {
 	}
 }
 
+func TestLoggerCategorizedLogLevels(t *testing.T) {
+	var b bytes.Buffer
+	logger := Logger{Level: DebugLevel, Writer: &IOWriter{Writer: &b}}
+
+	// Categorized logger must be indepented of base logger
+	cat1Logger := logger.Categorized("cat1").SetLevel(TraceLevel)
+	logger.Debug().Msg("logger debug here")
+	cat1Logger.Debug().Msg("cat1Logger debug here")
+	if !strings.Contains(b.String(), `"logger debug here"`) {
+		t.Fatal("logger.Debug must be logged")
+	}
+	if !strings.Contains(b.String(), `"cat1Logger debug here"`) {
+		t.Fatal("cat1Logger.Debug must be logged")
+	}
+
+	// Changing loglevel on category must not change base logger
+	b.Reset()
+	cat1Logger.SetLevel(InfoLevel)
+	logger.Debug().Msg("logger debug here")
+	cat1Logger.Debug().Msg("cat1Logger debug here")
+	if !strings.Contains(b.String(), `"logger debug here"`) {
+		t.Fatal("logger.Debug must be logged")
+	}
+	if strings.Contains(b.String(), `"cat1Logger debug here"`) {
+		t.Fatal("cat1Logger.Debug must be logged")
+	}
+
+	// Loglevel on other categorized logger has own loglevel
+	b.Reset()
+	cat2Logger := logger.Categorized("cat2").SetLevel(TraceLevel)
+	cat2Logger.Trace().Msg("cat2Logger trace here")
+	if !strings.Contains(b.String(), `"cat2Logger trace here"`) {
+		t.Fatal("logger.Debug for category cat2 must be logged")
+	}
+}
+
 func TestLoggerErrorStack(t *testing.T) {
 	logger := Logger{Level: TraceLevel, Writer: &ConsoleWriter{ColorOutput: true}}
 	logger.Info().Err(errno(0)).Msg("log errno(0) here")
@@ -621,5 +657,22 @@ func BenchmarkLogger(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		logger.Info().Str("foo", "bar").Msgf("hello %s", "world")
+	}
+}
+
+func BenchmarkCategorizedLogger(b *testing.B) {
+	logger := Logger{
+		TimeFormat: TimeFormatUnix,
+		Level:      DebugLevel,
+		Writer:     IOWriter{io.Discard},
+	}
+
+	catLogger := logger.Categorized("one")
+	catLogger.SetLevel(DebugLevel)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		catLogger.Debug().Str("foo", "bar").Msgf("hello %s", "world")
 	}
 }
